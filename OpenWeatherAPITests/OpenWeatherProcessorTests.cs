75e7f91410606e9c403ffcb62f5fa258911bb359
Aziz Lahlimi <1902972@cshawi.ca>
2021-11-14 21:00:49 -0500

    T4 complété-1902972

--- OpenWeatherAPITests/OpenWeatherProcessorTests.cs
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -38,6 +38,19 @@ namespace OpenWeatherAPITests
             _sut.ApiKey = apiKey;
             await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetOneCallAsync());
         }
-       
+
+        //Afficher le message de l’exception dans l’erreur que le client http n’est pas initialisé
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public async Task GetCurrentWeatherAsync_IfApiHelperNotInitialized_ThrowArgumentException(string apiKey)
+        {
+            _sut = OpenWeatherProcessor.Instance;
+            ApiHelper.InitializeClient();
+            _sut.ApiKey = apiKey;
+            await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetCurrentWeatherAsync());
+        }
+
+
     }
 }
