3175bab307bbbb3153d624e6cf955db75a1bd8d9
Aziz Lahlimi <1902972@cshawi.ca>
2021-11-14 20:57:05 -0500

    T1 complété-1902972

--- OpenWeatherAPITests/OpenWeatherProcessorTests.cs
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -18,37 +18,7 @@ namespace OpenWeatherAPITests
             await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetOneCallAsync());
 
         }
-        //Afficher le message de l’exception dans l’erreur que ApiKey est vide ou null.
-        [Fact]
-        public async void GetCurrentWeatherAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
-        {
-            _sut = OpenWeatherProcessor.Instance;
-            _sut.ApiKey = "balabla";
-            ApiHelper.ApiClient = null;
-            await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetCurrentWeatherAsync());
-        }
-        //Afficher le message de l’exception dans l’erreur que le client http n’est pas initialisé.
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        public async void GetOneCallAsync_IfApiHelperNotInitialized_ThrowArgumentException(string apiKey)
-        {
-            _sut = OpenWeatherProcessor.Instance;
-            ApiHelper.InitializeClient();
-            _sut.ApiKey = apiKey;
-            await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetOneCallAsync());
-        }
-        //Afficher le message de l’exception dans l’erreur que le client http n’est pas initialisé
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        public async Task GetCurrentWeatherAsync_IfApiHelperNotInitialized_ThrowArgumentException(string apiKey)
-        {
-            _sut = OpenWeatherProcessor.Instance;
-            ApiHelper.InitializeClient();
-            _sut.ApiKey = apiKey;
-            await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetCurrentWeatherAsync());
-        }
+       
 
     }
 }
